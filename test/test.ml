3733caafdbf7edbf02d6e68058ea8c2cb438f961
Anton Bachin <antonbachin@yahoo.com>
2024-08-09 19:47:51 +0300

    Tests: avoid long lines

--- test/test.ml
+++ test/test.ml
@@ -206,7 +206,8 @@ let suites = [
     ("parse-select-html5" >:: fun _ ->
       let soup = page "html5" |> parse in
       let test selector expected_count =
-        assert_equal ~msg:selector  ~printer:string_of_int (soup $$ selector |> count) expected_count
+        assert_equal ~msg:selector ~printer:string_of_int
+          (soup $$ selector |> count) expected_count
       in
 
       test "nav" 1;
@@ -218,7 +219,8 @@ let suites = [
       test "footer" 1);
 
     ("parse-select-google" >:: fun _ ->
-      assert_equal ~printer:string_of_int (page "google" |> parse $$ "form[action]" |> count) 1);
+      assert_equal ~printer:string_of_int
+        (page "google" |> parse $$ "form[action]" |> count) 1);
 
     ("parse-error" >:: fun _ ->
       let soup = parse "<p></p>" in
@@ -288,7 +290,7 @@ let suites = [
     ("generalized-select" >:: fun _ ->
       let soup = page "list" |> parse in
       let test root selector expected_count =
-        assert_equal ~msg:selector ~printer:string_of_int 
+        assert_equal ~msg:selector ~printer:string_of_int
           (root |> select selector |> count) expected_count
       in
 
@@ -301,7 +303,8 @@ let suites = [
 
     ("select-attribute-operators" >:: fun _ ->
       let soup = "<form action=\"/continue\"></form>" |> parse in
-      assert_equal ~printer:string_of_int (soup $$ "form[action=/continue]" |> count) 1);
+      assert_equal ~printer:string_of_int
+        (soup $$ "form[action=/continue]" |> count) 1);
 
     ("select_one" >:: fun _ ->
       let soup = page "list" |> parse in
@@ -381,7 +384,8 @@ let suites = [
     ("matches-selector" >:: fun _ ->
       let soup = parse "<div> <p id='foo'>bar</p> </div>" in
       let elem = select_one "div p#foo" soup |> unwrap_option in
-      assert_bool "element matches selector" (matches_selector soup "div p#foo" elem)
+      assert_bool "element matches selector"
+        (matches_selector soup "div p#foo" elem)
     );
 
     ("fold_attributes" >:: fun _ ->
